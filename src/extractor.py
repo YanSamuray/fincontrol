ffc664f218e28cb65e87796b75935b0f1b2cb31b
Yan Samuray Siepamann Vieira <yansamuray@gmail.com>
2026-03-23 03:33:28 -0300

    Refatora função de extração de cartão de crédito do Nubank para melhorar a legibilidade e remove comentários desnecessários

--- src/extractor.py
+++ src/extractor.py
@@ -8,14 +8,10 @@ def extract_nubank_credit_card(file_path: str) -> pd.DataFrame:
     with open(file_path, 'rb') as fileobj:
         ofx = OfxParser.parse(fileobj)
     
-    # Capturando as transações e a data de fechamento/vencimento da fatura atual
     transactions = ofx.account.statement.transactions
-    
-    # NOVA LINHA: Pegamos a data final do extrato (que o Nubank usa como referência da fatura)
     data_fatura = ofx.account.statement.end_date
     
     data = []
-    
     for txn in transactions:
         data.append({
             'id_transacao': txn.id,       
@@ -23,9 +19,36 @@ def extract_nubank_credit_card(file_path: str) -> pd.DataFrame:
             'valor': txn.amount,          
             'descricao': txn.memo,        
             'tipo': txn.type,
-            'data_fatura': data_fatura    # NOVA COLUNA: Carimba a qual fatura essa compra pertence
+            'data_fatura': data_fatura,
+            'banco': 'NUBANK'
         })
         
-    df = pd.DataFrame(data)
+    return pd.DataFrame(data)
+
+def extract_bb_credit_card(file_path: str) -> pd.DataFrame:
+    """
+    Lê um arquivo OFX de cartão de crédito do Banco do Brasil e retorna um DataFrame.
+    """
+    with open(file_path, 'rb') as fileobj:
+        ofx = OfxParser.parse(fileobj)
     
-    return df
+    transactions = ofx.account.statement.transactions
+    
+    try:
+        data_fatura = ofx.account.statement.end_date
+    except AttributeError:
+        data_fatura = None
+        
+    data = []
+    for txn in transactions:
+        data.append({
+            'id_transacao': txn.id,       
+            'data_compra': txn.date,      
+            'valor': txn.amount,          
+            'descricao': txn.memo,        
+            'tipo': txn.type,
+            'data_fatura': data_fatura,
+            'banco': 'BB'
+        })
+        
+    return pd.DataFrame(data)
